dc9461095716d38f59157029749c0de5d8f50cbe
TONYDESKTOP\Tony <tonyatpeking@gmail.com>
2024-04-20 23:56:51 -0500

    completed till primitive_types4 blocked by book reading progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(total_apples: i32) -> i32 {
+    if total_apples > 40 {
+        total_apples
+    } else {
+        total_apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
